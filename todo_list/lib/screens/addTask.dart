eb031195d40351cc1c7f4c0a51df2a8198f58e04
ArshadRatul <ratularshad@gmail.com>
2023-08-03 15:11:40 +0600

    Flutter To-do list

---
+++ todo_list/lib/screens/addTask.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Taskboard extends StatefulWidget {
+  const Taskboard({super.key});
+
+  @override
+  State<Taskboard> createState() => _TaskboardState();
+}
+class _TaskboardState extends State<Taskboard> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
